8e6400478187991e6d1444daf348fb67816c78d8
twistlemonade <twistlemonade@gmail.com>
2018-09-11 20:13:12 +0300

    Added menubar

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,28 @@
-$(document).ready(function() {
-  $('.js-section').waypoint(function(direction) {
-    if (direction == "down") {
-      $('nav').addClass('sticky');
-    } else {
-      $('nav').removeClass('sticky');
-    }
-}, {
-  offset: '60px;'
-});
+// $(document).ready(function() {
+//   $('.js-section').waypoint(function(direction) {
+//     if (direction == "down") {
+//       $('nav').addClass('sticky');
+//     } else {
+//       $('nav').removeClass('sticky');
+//     }
+// }, {
+//   offset: '60px;'
+// });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
