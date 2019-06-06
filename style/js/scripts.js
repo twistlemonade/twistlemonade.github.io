6bdd990305ae8d5d255e0c1889f622ca8f8ba82b
Marius <twistlemonade@gmail.com>
2019-06-06 18:19:14 +0300

    no message

---
+++ style/js/scripts.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+    'use strict';
+    /*-----------------------------------------------------------------------------------*/
+    /*	LIGHTGALLERY
+	/*-----------------------------------------------------------------------------------*/
+    $('.light-gallery').lightGallery({
+        thumbnail: false,
+        selector: '.lgitem',
+        animateThumb: true,
+        showThumbByDefault: false,
+        download: false,
+        autoplayControls: false,
+        zoom: false,
+	    fullScreen: false,
+        thumbWidth: 100,
+        thumbContHeight: 80,
+        videoMaxWidth: '1000px'
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	IMAGE ICON HOVER
+    /*-----------------------------------------------------------------------------------*/
+    $('.overlay a').prepend('<span class="over"><span></span></span>');
+    /*-----------------------------------------------------------------------------------*/
+    /*	PROGRESS BAR
+    /*-----------------------------------------------------------------------------------*/
+    $('.progress-list .progress .bar').progressBar({
+        shadow: false,
+        percentage: false,
+        animation: true,
+        height: 12
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TAB COLLAPSE
+    /*-----------------------------------------------------------------------------------*/
+    $('#tab1').tabCollapse({
+        tabsClass: 'hidden-sm hidden-xs',
+        accordionClass: 'visible-sm visible-xs'
+    });
+    $('#tab1').on('shown-accordion.bs.tabcollapse', function() {
+        $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+        $('.panel-group').on('shown.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').addClass(' panel-active');
+        }).on('hidden.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').removeClass(' panel-active');
+        });
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOGGLE
+    /*-----------------------------------------------------------------------------------*/
+    $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+    $('.panel-group').on('shown.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').addClass(' panel-active');
+    }).on('hidden.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').removeClass(' panel-active');
+    });
+    
+    /*-----------------------------------------------------------------------------------*/
+    /*	PRETTIFY
+    /*-----------------------------------------------------------------------------------*/
+    window.prettyPrint && prettyPrint();
+    /*-----------------------------------------------------------------------------------*/
+    /*	DATA REL
+    /*-----------------------------------------------------------------------------------*/
+    $('a[data-rel]').each(function() {
+        $(this).attr('rel', $(this).data('rel'));
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOOLTIP
+    /*-----------------------------------------------------------------------------------*/
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip();
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	VANILLA
+    /*-----------------------------------------------------------------------------------*/
+    var myForm;
+    myForm = new VanillaForm($("form.vanilla-form"));
+    /*-----------------------------------------------------------------------------------*/
+    /*	INSTAGRAM
+    /*-----------------------------------------------------------------------------------*/
+    var instagramFeed = new Instafeed({
+        target: 'instafeed-full',
+        get: 'user',
+        limit: 8,
+        userId: 48809923,
+        accessToken: '48809923.1677ed0.06b3fa8af1704405a5bbe179cba73085',
+        resolution: 'standard_resolution',
+        template: '<div class="item col-sm-3"><figure class="overlay instagram"><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></figure></div>',
+        after: function() {
+            $('#instafeed-full figure.overlay a').prepend('<span class="over"><span></span></span>');
+        }
+    });
+    $('#instafeed-full').each(function() {
+        instagramFeed.run();
+    }); 
+    /*-----------------------------------------------------------------------------------*/
+    /*	COUNTER UP
+    /*-----------------------------------------------------------------------------------*/
+    $('.fcounter').counterUp({
+        delay: 50,
+        time: 1000
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COLLAGEPLUS
+    /*-----------------------------------------------------------------------------------*/
+    collage();
+
+
+    function collage() {
+        $('#collage-medium').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 300,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+    };
+    
+    
+    
+    // This is just for the case that the browser window is resized
+    var resizeTimer = null;
+    $(window).on('resize', function() {
+        // hide all the images until we resize them
+        $('.collage .collage-image-wrapper').css("opacity", 0);
+        // set a timer to re-apply the plugin
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collage, 200);
+    });
+});
